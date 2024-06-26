1512181079bddede337bdb51360811c4a7349555
Yuyuko1024 <dingwenxuan4@gmail.com>
2024-06-26 08:24:46 +0800

    product: Do not build AOSP packages on exthm build

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -23,16 +23,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 # /product packages
 PRODUCT_PACKAGES += \
     Calendar \
-    Camera2 \
     Contacts \
     DeskClock \
-    Gallery2 \
-    LatinIME \
     Music \
     OneTimeInitializer \
     preinstalled-packages-platform-handheld-product.xml \
     SettingsIntelligence \
     frameworks-base-overlays
+
+ifeq ($(EXTHM_BUILD),)
+PRODUCT_PACKAGES += \
+    LatinIME \
+    Gallery2 \
+    Camera2
+endif
 
 PRODUCT_PACKAGES_DEBUG += \
     frameworks-base-overlays-debug
